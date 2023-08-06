b26882510b8fdf9506b3974872812ca4b3c8c451
Iamneo <techadmin@iamneo.ai>
2023-08-06 22:38:18 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/repositories/TaskRepository.java
+++ springapp/src/main/java/com/examly/springapp/repositories/TaskRepository.java
@@ -1,3 +1,18 @@
-public class TaskRepository {
+package com.examly.springapp.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.examly.springapp.entities.*;
+
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, String> {
+
+    Task findByTaskId(String taskId);
+
+    List<Task> findByTaskHolderName(String taskHolderName);
     
 }
